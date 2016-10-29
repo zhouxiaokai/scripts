662688adb1086f6ad57839f167da65609ce281a9
zhouxiaokai <1019972897@qq.com>
2016-10-29 14:16:01 +0800

    Update nginx.sh
    independ scripts for nginx config file

--- lnmp/nginx.sh
+++ lnmp/nginx.sh
@@ -1,1 +1,48 @@
 #!/bin/sh
+
+help(){
+  echo "$0 [server]  [port]  [opts]"
+  echo "$0 [http]  [opts]"
+  exit 1
+}
+
+help_server()
+{
+   echo "$0 [server]  [port]  [opts]"
+   exit 1
+}
+
+[ $# -lt 2 ] && help
+
+
+generate_server_https()
+{
+
+}
+
+generate_server_http(){
+  echo "http"
+}
+
+
+generate_server(){
+   echo "$0 $@"
+   case $1 in 
+       443) generate_server_https;;
+       *)   generate_server_http;;
+   esac
+}
+
+generate_server_http(){
+  echo "http"
+}
+
+case $1 in
+help) help;;
+server) [ $# -lt 3 ] && help_server
+       generate_server $@
+    ;;
+http) echo "generate for http"
+       generate_http
+    ;;
+esac
