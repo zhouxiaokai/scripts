fd7ca749589882438fc810468b9ba7eba6a81783
zhouxiaokai <1019972897@qq.com>
2016-10-29 14:25:50 +0800

    update something

--- lnmp/nginx.sh
+++ lnmp/nginx.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 help(){
-  echo "$0 [server]  [port]  [opts]"
-  echo "$0 [http]  [opts]"
+  echo $0
+  echo "	[server]  [port]  [opts]"
+  echo "	[http]  [opts]"
   exit 1
 }
 
 help_server()
 {
-   echo "$0 [server]  [port]  [opts]"
+   echo "$0  server"
+   echo "  [port]  [ -r root ] [ -i index ]"
    exit 1
 }
-
-[ $# -lt 2 ] && help
 
 
 generate_server_https()
 {
-
+ echo "https"
 }
 
 generate_server_http(){
@@ -26,8 +26,8 @@ generate_server_http(){
 
 
 generate_server(){
-   echo "$0 $@"
-   case $1 in 
+   echo " $@"
+   case $2 in 
        443) generate_server_https;;
        *)   generate_server_http;;
    esac
@@ -39,10 +39,11 @@ generate_server_http(){
 
 case $1 in
 help) help;;
-server) [ $# -lt 3 ] && help_server
+server) [ $# -lt 2 ] && help_server
        generate_server $@
     ;;
 http) echo "generate for http"
        generate_http
     ;;
+*) help ;;
 esac
