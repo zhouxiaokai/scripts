a07248309e0e71249be6ee42c0b6c9ed1b04a436
zhouxiaokai <1019972897@qq.com>
2016-10-29 16:57:43 +0800

    Update nginx.sh

--- lnmp/nginx.sh
+++ lnmp/nginx.sh
@@ -18,6 +18,34 @@ help_server()
 generate_server_https()
 {
  echo "https"
+ echo '
+#
+# HTTPS server configuration
+#
+server {
+    listen       443;
+    server_name  120.27.138.55;
+    ssl                  on;
+    ssl_certificate      /etc/nginx/server.crt;
+    ssl_certificate_key  /etc/nginx/server.key;
+
+    ssl_session_timeout  5m;
+
+#    ssl_protocols  SSLv2 SSLv3 TLSv1;
+#    ssl_ciphers  ALL:!ADH:!EXPORT56:RC4+RSA:+HIGH:+MEDIUM:+LOW:+SSLv2:+EXP;
+#    ssl_prefer_server_ciphers   on;
+
+    location / {
+     root   /home/wwwroot/www;
+     index  testssl.html index.html index.htm index.php;
+     include enable-php.conf;
+     proxy_redirect off;
+     proxy_set_header Host $host;
+     proxy_set_header X-Real-IP $remote_addr;
+     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+     #proxy_pass http://120.27.138.55/ssl/;
+    }
+}' > $1/vhost/443.conf
 }
 
 generate_server_http(){
