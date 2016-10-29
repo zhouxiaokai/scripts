4ecd45028a58417c8fdf4039bff1da99a223cbaa
zhouxiaokai <1019972897@qq.com>
2016-10-29 18:10:25 +0800

    Update nginx.sh

--- lnmp/nginx.sh
+++ lnmp/nginx.sh
@@ -2,7 +2,7 @@
 
 help(){
   echo $0
-  echo "	[server]  [port]  [opts]"
+  echo "	[server] [host] [port]  [opts]"
   echo "	[http]  [opts]"
   exit 1
 }
@@ -10,7 +10,7 @@ help(){
 help_server()
 {
    echo "$0  server"
-   echo "  [port]  [ -r root ] [ -i index ]  path "
+   echo " [host] [port]  [ -r root ] [ -i index ]  path "
    exit 1
 }
 
@@ -19,18 +19,21 @@ generate_server_https()
 {
  echo "https"
  [ -d $1/vhost ] || mkdir -p $1/vhost
- echo '
+ conf=$1/vhost/443.conf
+ server_name=$3
+ echo "
 #
 # HTTPS server configuration
 #
 server {
     listen       443;
-    server_name  120.27.138.55;
+    server_name  $server_name;
     ssl                  on;
     ssl_certificate      /etc/nginx/server.crt;
     ssl_certificate_key  /etc/nginx/server.key;
 
-    ssl_session_timeout  5m;
+    ssl_session_timeout  5m;" > $conf
+ echo '   
 
 #    ssl_protocols  SSLv2 SSLv3 TLSv1;
 #    ssl_ciphers  ALL:!ADH:!EXPORT56:RC4+RSA:+HIGH:+MEDIUM:+LOW:+SSLv2:+EXP;
@@ -44,7 +47,7 @@ server {
      proxy_set_header Host $host;
      proxy_set_header X-Real-IP $remote_addr;
      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-     #proxy_pass http://120.27.138.55/ssl/;
+     #proxy_pass http://hostip/ssl/;
     }
 }' > $1/vhost/443.conf
 }
@@ -52,11 +55,11 @@ server {
 generate_server_http(){
   [ -d $1/vhost ] || mkdir -p $1/vhost
   conf=$1/vhost/$2.conf
- 
- echo ' server
-    {'  >  $conf
-  echo "  listen $2 default_server;
-        server_name www.lnmp.org;
+  server_name=$3
+  echo " server
+        {
+        listen $2 default_server;
+        server_name $3;
         index index.html index.htm index.php;
         root  /home/wwwroot/admin;
         #error_page   404   /404.html;
@@ -89,7 +92,9 @@ location ~ /services/.*$ {
             set $rule_0 1$rule_0;
         }
         if ($rule_0 = "1"){
-            rewrite /(.*) https://120.27.138.55/$1 permanent;                       break;
+            rewrite /(.*) ' >>$conf 
+ echo -e "https://$server_name/\c" >>$conf 
+ echo '$1 permanent;                       break;
         }
     }
 
@@ -103,11 +108,12 @@ generate_server(){
    [ $# -lt 3 ] &&  help_server
    port=$2
    path=$3
+   host=$4
   echo "path=$path port=$port"
    case $2 in 
-       443) generate_server_https $path;;
+       443) generate_server_https $path $port $host;;
        *)   echo "test $@i";
-            generate_server_http  $path $port ;
+            generate_server_http  $path $port $host ;
           ;;
 
    esac
@@ -116,7 +122,7 @@ generate_server(){
 
 case $1 in
 help) help;;
-server) [ $# -lt 2 ] && help_server
+server) [ $# -lt 4 ] && help_server
        generate_server $@;
     ;;
 http) echo "generate for http"
