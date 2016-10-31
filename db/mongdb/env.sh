4e987a806c23991a039e5ac82196260b92ffe0dc
zhouxiaokai <1019972897@qq.com>
2016-10-31 10:09:23 +0800

    update env.sh with function and command

--- db/mongdb/env.sh
+++ db/mongdb/env.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+gen_repo(){
+
 [ -f /etc/yum.repos.d/mongodb-org-3.4.repo ] || {
 echo '
 [mongodb-org-3.4]
@@ -8,5 +10,41 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/testing/x86_
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc' > /etc/yum.repos.d/mongodb-org-3.4.repo
+}
+}
+
+install(){
 
 yum install -y mongodb-org
+
+}
+
+
+remove()
+{
+	service mongod stop
+        sudo yum erase $(rpm -qa | grep mongodb-org)
+        rm -r /var/log/mongodb
+        rm -r /var/lib/mongo
+}
+
+start()
+{
+ 
+}
+
+help ()
+{
+ echo "$0 [repo|install|remove]"
+}
+
+
+case $1 in
+
+repo) gen_repo;;
+install) install ;;
+remove) ;;
+start) ;;
+*) help;;
+
+esac
