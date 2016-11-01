878a1c5d6fcd0515459a61b3ed07777a5fdf8440
zhouxiaokai <1019972897@qq.com>
2016-11-01 20:26:06 +0800

    add dbninja install

---
+++ db/mysql/dbninja.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+
+tdir=$2
+
+help(){
+ echo "$0 [target dir]"
+ exit 1
+}
+
+
+install(){
+[ -d $tdir/dbninja ] || {
+   wget http://dbninja.com/download/dbninja.tar.gz -O /tmp/dbninja.tar.gz || {
+         echo "Downloading from http://dbninja.com/download/dbninja.tar.gz failed"
+         exit 1
+   }
+   tar -xzvf /tmp/dbninja.tar.gz -C $tdir
+   sudo chmod 775 $tdir/dbninja/_users
+
+#http://www.dbninja.com/?page=resources&z=102
+   yum -y install  php-mysqli
+   yum --enablerepo=remi install php-pecl-jsonc
+   user=` ps -eo ruser,rgroup,command |egrep "apache|nginx|lighttpd" |egrep -m 1 -v "root|grep" | awk '{print $1}'`
+   group=` ps -eo ruser,rgroup,command |egrep "apache|nginx|lighttpd" |egrep -m 1 -v "root|grep" | awk '{print $2}'`
+   chown -R $user:$group $tdir/dbninja 
+}
+}
+
+test(){
+	ps -eo ruser,rgroup,command |egrep "apache|nginx|lighttpd" |egrep -m 1 -v "root|grep"
+}
+
+case $1 in
+   install) 
+
+	[ -d "$tdir" ] || help
+             install;;
+   test)test ;;
+esac
+
+
