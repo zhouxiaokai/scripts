010b062ffe9d4543bbd94000c882100fd1fae79f
zhouxiaokai <1019972897@qq.com>
2016-11-08 20:09:34 +0800

    update

--- npm/npm.sh
+++ npm/npm.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm    
+wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+rpm -ivh epel-release-6-8.noarch.rpm
+rpm -ivh remi-release-6.rpm
  npm config set registry https://registry.npm.taobao.org
 npm install -g nrm --registry=https://registry.npm.taobao.org
 nrm ls
